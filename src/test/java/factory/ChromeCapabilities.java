d4787b2e77f970f7bd3c06a64a3f20b21d7eb539
Ramses Almeida <rsaccoll@gmail.com>
2020-08-24 10:48:23 -0300

    add more itens to the capabilities

--- src/test/java/factory/ChromeCapabilities.java
+++ src/test/java/factory/ChromeCapabilities.java
@@ -23,6 +23,8 @@ public class ChromeCapabilities implements ICapabilities {
     public void setCapabilities() {
         options.addArguments("incognito");
         options.addArguments("start-maximized");
+        options.addArguments("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         this.caps.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
         this.caps.setAcceptInsecureCerts(true);
         this.caps.setJavascriptEnabled(true);
